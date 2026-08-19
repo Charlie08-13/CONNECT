bd36b07ea8f3fc0ef9d01a4fe627724f244e4339
Charlie08-13 <ashwini2006kashyap@gmail.com>
2026-08-19 21:44:24 +0530

    Update project changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,9 @@ import {
 // API CONFIG
 // =========================================
 
-const API_URL = "http://localhost:5000";
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 
 const Login = () => {
   const navigate = useNavigate();
